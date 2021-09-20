fc35c71f170c9544e6cb899dafb6af7347765283
RajPrabhakar <RajPrabhakar1804@gmail.com>
2021-09-20 23:05:51 +0530

    allowed host

--- strikers/settings.py
+++ strikers/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*',]
 
 # STRIPE_SECRET_KEY = Secret.STRIPE_SECRET_KEY
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY')
